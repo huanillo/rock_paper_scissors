4b8031b8215781a5cc38e60c3a92283d2494e903
huanillo <102235967+huanillo@users.noreply.github.com>
2025-03-03 22:31:47 +0100

    Finished program. Had to use objects in order to make it work properly.

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function getHumanChoice(){
 function  playRound(){
 
     let human = getHumanChoice();
-    if ((human != "paper") && (human != "rock") && (human != "scissors")){  // No prompt, we finish
+    if ((human !== "paper") && (human !== "rock") && (human !== "scissors")){
         return "No valid choice made.";  
     }
     let computer = getComputerChoice();
@@ -52,28 +52,51 @@ function  playRound(){
     console.log("Computer chose:", computer);
 
     if (human === "rock" && computer === "paper"){
-        return "YOU LOSE! Paper beats rock";
+        return { message: "YOU LOSE! Paper beats rock", result: 0 };
     }
     else if (human === "rock" && computer === "scissors"){
-        return "YOU WIN! Rock beats scissors";
+        return { message: "YOU WIN! Rock beats scissors", result: 1 };
     }
     else if (human === "paper" && computer === "rock"){
-        return "YOU WIN! Paper beats rock";
+        return { message: "YOU WIN! Paper beats rock", result: 1 };
     }
     else if (human === "paper" && computer === "scissors"){
-        return "YOU LOSE! Scissors beats papers";
+        return { message: "YOU LOSE! Scissors beats papers", result: 0 };
     }
     else if (human === "scissors" && computer === "rock"){
-        return "YOU LOSE! Rock beats scissors";
+        return { message: "YOU LOSE! Rock beats scissors", result: 0 };
     }
     else if (human === "scissors" && computer === "paper"){
-        return "YOU WIN! Scissors beats paper";
+        return { message: "YOU WIN! Scissors beats paper", result: 1 };
     }
     else {
-        return "TIE!";
+        return { message: "TIE!", result: 2 };
     }
 }
-
-console.log(playRound());
 //TRACK players score. Functions name: humanScore and computerScore. 
 //Initialize those variables with a value of 0.
+
+//CREATE function called playGame that plays 5 rounds and tracks the scores.
+function playGame(){
+    let round = 0;
+    let humanScore = 0;
+    let computerScore = 0;
+
+    while (round < 5 ){
+        let result = playRound();  // Get the result of the round
+        console.log(result.message);  // Show the message of the round
+        if (result.result === 1){
+            humanScore ++;
+        }
+        else if (result.result === 0){
+            computerScore++;
+        }
+        else{
+            computerScore++;
+            humanScore++;
+        }
+            round++
+        }
+    return 'FINAL SCORE: \n Human: ' + humanScore + '\n Computer:' + computerScore;
+}
+console.log(playGame());
